b44ff4e36e91dd0c6350050f9aa992a6df7f9935
root <root@DESKTOP-O1KC17T>
2023-03-23 15:53:39 +0200

    add UserLibrary model

--- djgumroad/users/models.py
+++ djgumroad/users/models.py
@@ -2,7 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db.models import CharField
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
-
+from django.db import models
+from products.models import Product
 
 class User(AbstractUser):
     """
@@ -24,3 +25,29 @@ class User(AbstractUser):
 
         """
         return reverse("users:detail", kwargs={"username": self.username})
+
+class UserLibrary(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name='library')
+    products = models.ManyToManyField(Product ,blank=True)
+
+    class Meta:
+        verbose_name_plural = 'UserLibraries'
+    
+    def __str__(self):
+        return self.user.email
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
