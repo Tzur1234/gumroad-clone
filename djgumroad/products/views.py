722336ba4716a90382779529ce4eed482e158f0d
root <root@DESKTOP-O1KC17T>
2023-04-02 16:50:55 +0300

    add new staff

--- djgumroad/products/views.py
+++ djgumroad/products/views.py
@@ -195,8 +195,9 @@ def StripeWebhookView(request):
 
     payload = request.body    
     sig_header = request.META["HTTP_STRIPE_SIGNATURE"]
-    endpoint_secret=settings.STRIPE_WEBHOOK_SECRET
-    print('endpoint_secret: ', endpoint_secret)
+    # endpoint_secret=settings.STRIPE_WEBHOOK_SECRET
+    endpoint_secret='whsec_o0T4qLvSeI0kjhpUs2JcHq9F0Ik9VUeY'
+    # print('endpoint_secret: ', endpoint_secret)
     
     try:
         event = stripe.Webhook.construct_event(
